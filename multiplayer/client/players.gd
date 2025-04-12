0ccd70a72636cd9165f9cfde5da8107f351a0eb5
mvrtza <morixplus@gmail.com>
2025-04-12 23:47:01 +0400

    first upload

---
+++ multiplayer/client/players.gd
@@ -0,0 +1,27 @@
+extends Node3D
+
+@export var local_player_scene: PackedScene
+
+
+func _ready() -> void:
+	multiplayer.connected_to_server.connect(create_local_player)
+
+
+func create_local_player() -> void:
+	var player: Node3D = local_player_scene.instantiate()
+	var peer_id: int = multiplayer.get_unique_id()
+	player.name = str(peer_id)
+	add_child(player)
+	if( player.get_node("ClientSync").is_inside_tree()):
+		var client_sync: MultiplayerSynchronizer = player.get_node("ClientSync")
+		client_sync.set_multiplayer_authority(peer_id)
+		client_sync.set_visibility_for(1, true)
+
+
+func _on_player_spawner_spawned(node: Node) -> void:
+	%Debug.add_visible_peer(node.name)
+
+
+func _on_player_spawner_despawned(node: Node) -> void:
+	%Debug.remove_visible_peer(node.name)
+
